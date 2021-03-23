05dd28d3e88edf073beb9933bcd9b4603e96245f
saumyasri005@gmail.com <Saumya-bit>
2021-03-23 15:52:19 +0530

    final commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestMSExercise45
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private double squareNo;
+        private double cubeNo;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            maths.Calculation objc = new maths.Calculation();
+            double num1 = 24;
+            double squareNO = num1 * num1;
+            double squarerootNo = Math.Sqrt(num1);
+            double cubeNO = num1 * num1 * num1;
+
+            Assert.AreEqual(squareNo, objc.Square(num1));
+            Assert.AreEqual(squarerootNo, objc.SquareRoot(num1));
+            Assert.AreEqual(cubeNo, objc.Cube(num1));
+
+        }
+    }
+}
---
+++ Properties/Maths.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace maths
+
+{
+    internal class Calculation
+    {
+        internal double Square(double num1)
+        {
+            double no = 0;
+            try
+            {
+                no = num1 * num1;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error" + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("End of program");
+            }
+
+            return no;
+        }
+
+        internal double SquareRoot(double num2)
+        {
+            double no = 0;
+            no = Math.Sqrt(num2);
+            return no;
+
+        }
+
+        internal double Cube(double num1)
+        {
+            double no = 0;
+            no = num1 * num1 * num1;
+            return no;
+        }
+    }
+}
